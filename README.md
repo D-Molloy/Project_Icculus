4f55cd7fa4bfc8fe24bc7ab3e8f74d272aaa1555
Denis Molloy <denis.molloy@gmail.com>
2018-09-18 11:30:04 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project_Icculus
+
+https://github.com/bradtraversy/devconnector
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res)=> res.send("HELLO!!!"))
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Serve running on PORT ${PORT}`))
