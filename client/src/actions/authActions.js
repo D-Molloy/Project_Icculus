e62fe0f3ff6e40fe14aa11bd5b78fd4f33dffeca
Denis Molloy <denis.molloy@gmail.com>
2018-10-31 19:37:22 -0700

    setup SET_CURRENT_USER action/reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
-import { GET_ERRORS } from "./types";
 import axios from "axios";
-//REGISTER USER
+import jwt_decode from "./jwt_decode";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //history param was added when withRouter was implemented in Register.js
 
@@ -14,4 +15,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//  Login - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+      // set token to localstorage
+      localStorage.setItem("jwtToken", token);
+      //set token to auth header
+      /// token includes user data - need jwtDecode to retrieve the info
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,7 @@
+import { SET_CURRENT_USER } from "../actions/types";
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from "constants";
+import isEmpty from "../validation/is_empty";
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +9,13 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        // if action.payload is empty, the user wasn't authenticated
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
