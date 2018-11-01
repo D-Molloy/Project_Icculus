5a40b0f8bff686dea94dd7fc23fe665837a50b97
Denis Molloy <denis.molloy@gmail.com>
2018-11-01 15:28:07 -0700

    implemented Login form functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,29 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// auth
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 //custom components
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
 import "./App.css";
+
+// check for token
+if (localStorage.jwtToken) {
+  //set the auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -38,6 +38,9 @@ class Register extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
     }
