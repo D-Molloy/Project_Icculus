f176ee197e6af3fe0e99f302338d6342daa8ca9c
Denis Molloy <denis.molloy@gmail.com>
2018-11-12 23:23:23 -0800

    added Post component and hooked up addPost action/reduce

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+//components
+import Spinner from "../common/Spinner";
+import PostForm from "./PostForm";
+
+class Posts extends Component {
+  render() {
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Posts;
