0e2d75011987ce896c3e17780a9d8d7690ab9b0e
Denis Molloy <denis.molloy@gmail.com>
2018-11-14 13:47:33 -0800

    added like/unlike functionality to PostItem

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST,
+  LIKE_POST
+} from "./types";
 
 //Add a post
 export const addPost = postData => dispatch => {
@@ -38,6 +45,85 @@ export const getPosts = () => dispatch => {
         payload: null
       })
     );
+};
+
+// Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// // Like a post
+// export const addLike = id => dispatch => {
+//   axios
+//     .post(`/api/posts/like/${id}`)
+//     .then(res => dispatch(getPosts()))
+//     .catch(err =>
+//       dispatch({
+//         type: GET_ERRORS,
+//         payload: err.response.data
+//       })
+//     );
+// };
+
+// // Remove a like
+// export const removeLike = id => dispatch => {
+//   axios
+//     .post(`/api/posts/unlike/${id}`)
+//     .then(res => dispatch(getPosts()))
+//     .catch(err =>
+//       dispatch({
+//         type: GET_ERRORS,
+//         payload: err.response.data
+//       })
+//     );
+// };
+//Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(({ data }) => {
+      dispatch({
+        type: LIKE_POST,
+        payload: data //pass in updated post
+      });
+    })
+    .catch(({ response }) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: response.data
+      });
+    });
+};
+
+//Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(({ data }) => {
+      dispatch({
+        type: LIKE_POST,
+        payload: data //pass in updated post
+      });
+    })
+    .catch(({ response }) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: response.data
+      });
+    });
 };
 
 // set loading state
